010fa3470d1b9a4aed0289cb1eaaa9c5db3d4551
Anish8412 <anishk70925@gmail.com>
2026-01-31 08:00:35 +0000

    variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 133333
+let accountEmail = "anishk70925@gmail.com"
+var accountPassword = "23334"
+ accountcity = "jaipur "
+let accounState;
+
+accountEmail = "rahul@gmail.com"
+accountPassword = "2344221"
+accountCity = "faridabad"
+console.log(accountId)
+/*
+prefer not to use var
+because of issue in block scope amd functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
