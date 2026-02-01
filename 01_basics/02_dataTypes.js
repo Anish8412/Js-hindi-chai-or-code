2102e951c243deb03a6d20045a370816927d7a4a
Anish8412 <anishk70925@gmail.com>
2026-02-01 11:10:38 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";// Treat all js code as newer version
+
+//alert ( 3 + 4) // we are using nodejs, not browser
+
+console.log ( 3 
+    + 4);// code readablity should be high
+
+console.log("anish")
+
+
+// datatypes
+
+let name = "Anish"
+let age = 18
+let isLoggedIn = false
+
+
+
+// number =>2 to power 53
+//bigint = use in longest number
+//boolean => true/false
+// null  => standalone value
+//undefined => not value assign in code 
+// symbol =>unique 
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null) // object
