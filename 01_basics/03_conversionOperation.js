a32a7ccc1713f7736da49070a0871d50464ef051
Anish8412 <anishk70925@gmail.com>
2026-02-02 10:23:30 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,8 +21,38 @@
 //"Anish" => true
 
 
-let someNumber = 33
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//************************ operation **************
+
+
+//let value = 3
+//let negValue = -value
+//console.log(negValue);
+
+
+
+let str1 = "ujjawal"
+let str2 = " sudhanshu"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + 3) ;
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 3);
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true);
+
+let gameCounter = 100
+
+++gameCounter;
+console.log(gameCounter);
+
