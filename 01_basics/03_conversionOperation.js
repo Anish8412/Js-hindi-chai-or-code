2458045241e24cefeeec5880e5abfa4e0d820d47
Anish8412 <anishk70925@gmail.com>
2026-02-02 09:36:46 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+//let score = false;
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+//let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber)
+
+//"33" => 33
+//"33abc" =>NaN (not a Number)
+//true => 1
+//false=> 0
+
+
+
+//conversion
+//1 => true; 0 => false
+//"" => false
+//"Anish" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
