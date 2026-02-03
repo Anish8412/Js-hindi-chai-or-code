0647ff2bf00fadd2849b811254180dc4fa007ff5
Anish8412 <anishk70925@gmail.com>
2026-02-03 11:14:40 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+//  primitive datatype
+
+// 7 types : string , Number, Boolean, Null, undefined,symbol,bigInt
+
+
+//Array, Objects, function
+
+ const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber =122345678897544322;
+console.log('bigNumber');
+
+
+// const heros = ["shaktiman","naagraj" "doga" ];
+// let myObj ={
+//     name: "hitesh",
+//     age: 22,
+// }
+
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof myFunction);
+
+
