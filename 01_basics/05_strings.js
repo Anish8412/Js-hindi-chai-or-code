25b8d5f0864ae82e614add4a9daab0a4da4cc92b
Anish8412 <anishk70925@gmail.com>
2026-02-04 10:01:45 +0000

    05_strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = "hitesh"
+const repoCount = 20
+const age = 30
+
+//console.log(name + repoCount + 'value' + age )
+
+
+// console.log(`hello my name is ${name} and my repo count is $ {repoCount}`);
+
+ const gameName = new String('hiteshhc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+
+
+const newStringOne = "    hitesh   "
+console.log(newStringOne);
+console.log(newString.trim());
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+
+console.log(url.includes('sundar'))
+
+
+console.log(gameName.split('-'));
