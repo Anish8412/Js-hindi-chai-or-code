97f1162174cf1065fc6611009668a32f089ac97b
Anish8412 <anishk70925@gmail.com>
2026-02-08 06:13:51 +0000

    02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -2,7 +2,31 @@ const marvel_heros = ["thor", "Ironman", "spidermen"]
 const dc_heros = ["superman", "flash", "batman"]
 
 
-marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)
 
-console.log(marvel_heros);
-console.log(marvel_heros[3][1]);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);////same value aati hai dono use me push ya  concat karne par
+// marvel_heros.concat(dc_heros)
+// console.log(marvel_heros);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_new_heros =[...marvel_heros, ...dc_heros]
+// console.log(all_new_heros);
+
+const another_array = [1, 2 , 3, [4, 5, 6], 7, [6, 7,[4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Anish"))
+console.log(Array.from("anish"))
+console.log(Array.from({name:"Anish"}))///interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
