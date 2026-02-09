cc2e482b3e94a095ab1517f20ae282cd52401a1a
Anish8412 <anishk70925@gmail.com>
2026-02-09 07:08:13 +0000

    04_objects.js

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// // const tinderuser = new Object()   // single tern object hai
+// const tindrUser ={}// non singleton object hai
+// // console.log(tindrUser);
+
+
+// tinderUser.id ="123abn"
+// tinderUser.name = "Anish"
+// tinderUser. isloggedIn = false;
+
+// // console.log(tinderUser)
+
+// const regularUser = {
+//     email: "anish@gmail.com",
+//     fullname: {
+//         userfullname:{
+//             firstname: "anish",
+//             lastname: "kumar"
+//         }
+//     }
+
+
+// }
+
+// console.log(regularUser.fullname.userfullname.firstname)
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a",6: "b"}
+// // const obj3 = Object.assign({},  obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+const users = [
+
+{
+  id : 1,
+  email: "amishj@gmail.com",
+},
+{
+  id : 1,
+  email: "amishj@gmail.com",
+},
+]
+
+users[1]. email
+
+console.log(tinderUser);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('islogged'));
+
+
