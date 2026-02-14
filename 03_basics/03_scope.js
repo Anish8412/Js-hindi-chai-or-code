9885c5e7531b3284141b5569fe9818f6ccc6472d
Anish8412 <anishk70925@gmail.com>
2026-02-14 11:14:55 +0000

    03_scope.js

---
+++ 03_basics/03_scope.js
@@ -0,0 +1,19 @@
+var c = 300/// global scope
+
+
+if (true){  ////block scope
+    let a = 10
+    const b = 30
+    console.log("INNER: ", a);
+
+}
+
+
+
+
+
+
+
+// console.log(a);
+// console.log(b);
+console.log(c);
