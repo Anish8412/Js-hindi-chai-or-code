25b2002ed7c942b4b394557ab6cc44f0752de553
Anish8412 <anishk70925@gmail.com>
2026-02-14 12:22:38 +0000

    03_scope.js

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -1,12 +1,12 @@
-var c = 300/// global scope
+// var c = 300/// global scope
 
 
-if (true){  ////block scope
-    let a = 10
-    const b = 30
-    console.log("INNER: ", a);
+// if (true){  ////block scope
+//     let a = 10
+//     const b = 30
+//     console.log("INNER: ", a);
 
-}
+// }
 
 
 
@@ -16,4 +16,55 @@ if (true){  ////block scope
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+// function one (){
+//     const username = "Anish"
+
+//     function two (){
+//         const website = "youtube"
+//         console.log(username)
+//     }
+//     // console.log(website);
+
+//     two()
+// }
+
+// one()
+
+
+// if (true) {
+//     const username = "Anish"
+
+//     if (username === "Anish") {
+//         const website = "youtube"
+//         // console.log(username + website);
+//     }
+// }
+
+// console.log(website);
+ 
+
+// console.log(username);
+
+
+
+
+
+//////++++++++++++++++interesting +++++++++++++++++++++++++++++++++
+
+addone(5)
+function addone(num){
+    return num + 1              // 1.techniq function definiing
+}
+
+
+// const addTwo =  function(num){
+//     return num + 2
+// }                                2. method defining function
+                               
+// addTwo(5)
+
+
+
