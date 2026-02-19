f3e760cf54c8bd0513617aae12e01389d5ed1233
Anish8412 <anishk70925@gmail.com>
2026-02-19 10:42:58 +0000

    truthy.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -11,3 +11,34 @@ if (userEmail){
 // falsy value
 
 // false, 0, -0, bigInt 0n, "",null, undefined, NaN
+
+
+if (userEmail.length === 0){
+    console.log("Array is Empty");
+}
+
+// if (Object.keys(emptyObj).length=== 0){
+//     console.log("object is empty");
+
+// }
+// false ==0 true  false == '' true,  0 == true
+
+// nulish coalescing operator (??)  :null undefine
+let val1;
+
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+// var1 = undefined ?? 15
+ val1 = null ?? 10 ?? 20
+ console.log(val1)
+
+
+
+//  Terniary operator
+
+// condition ? true : false
+
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80"): console.log("more than 80")
