d620f9832e89a949326cd31162c1a1ee3fd6b308
Anish8412 <anishk70925@gmail.com>
2026-02-21 12:26:15 +0000

    05_itteration

---
+++ 05_itteration/four.js
@@ -0,0 +1,37 @@
+//object
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'Ruby',
+    swift: "swift by apple"
+
+}
+
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for${myObject [key]}`);
+ 
+}
+
+
+const programming = ['js','rb','py','java','cpp']
+
+for (const key in programming) {
+
+   console.log(programming [key]);
+}
+
+
+// const map = new Map()
+
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('fr', "france")
+
+
+// for (const key in map) {
+//    console.log('key')
+    
+    
+// }
